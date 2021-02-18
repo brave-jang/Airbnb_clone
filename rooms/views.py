9e156065436e42762508d3830e0e0605bc20e998
bravejang <wkdrmfmt@gmail.com>
2021-02-18 14:23:24 +0900

    View FBV기반의 페이지 작성

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
-import math
 from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import redirect, render
 from . import models
 
 
@@ -12,3 +13,11 @@ class HomeView(ListView):
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, 'rooms/detail.html', {"room":room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
