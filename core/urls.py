979cdc433b871aaeadc8d65afa50650dd3154195
bravejang <wkdrmfmt@gmail.com>
2021-02-18 12:11:02 +0900

    Class Based Views

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,5 @@ app_name = "core"
 
 
 urlpatterns = [
-    path("", rooms_views.all_rooms, name="home")
+    path("", rooms_views.HomeView.as_view(), name="home")
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,25 +1,14 @@
 import math
-from django.shortcuts import redirect, render
-from django.core.paginator import EmptyPage, Paginator
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    end_page = math.ceil(paginator.count/10)
-    try:
-        rooms = paginator.page(int(page))
-        if int(page) % 5 == 0:
-            start = (int(int(page) / 5) * 5) - 5
-            end = int(page)
-            paginator_range = paginator.page_range[start:end]
-        else:
-            start = (int(int(page) / 5) * 5)
-            end = (math.ceil(int(page) / 5) * 5)
-            paginator_range = paginator.page_range[start:end]
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
 
-    return render(request, "rooms/home.html", {"page": rooms, "paginator_range":paginator_range})
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
