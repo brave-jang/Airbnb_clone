8f48c046e18a83fcfc117df437ce6efc636ab100
bravejang <wkdrmfmt@gmail.com>
2021-03-01 22:44:37 +0900

    예약 기능 완성

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls", namespace="core")),
     path("users/", include("users.urls", namespace="users")),
+    path("reservations/", include("reservations.urls", namespace="reservations")),
     path('rooms/', include('rooms.urls', namespace='rooms'))
 ]
 
